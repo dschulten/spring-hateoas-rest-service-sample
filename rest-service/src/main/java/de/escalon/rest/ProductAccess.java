67b07578d47dbc9477f2203d6cc7d55950fd66d7
Dietrich Schulten <ds@escalon.de>
2012-10-07 21:01:14 +0200

    some bugs in ProductController and ProductAccess

--- rest-service/src/main/java/de/escalon/rest/ProductAccess.java
+++ rest-service/src/main/java/de/escalon/rest/ProductAccess.java
@@ -1,18 +1,30 @@
 package de.escalon.rest;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductAccess {
 
+    private List<Product> products = Arrays.asList(new Product(1L,
+            "Starfleet Standard Tricorder"), new Product(2L, "Flux Capacitor"),
+            new Product(3L, "Phase Calibrator"));
+
     public Iterable<? extends Product> getProductsOfPerson(Long personId) {
 
-        return Arrays.asList(new Product(1L,
-                "Starfleet Standard Tricorder"), new Product(2L,
-                "Flux Capacitor"), new Product(1L, "Phase Calibrator"));
+        if (personId == 1L)
+            return products.subList(0, 1);
+        else if (personId == 2L)
+            return products.subList(1, 2);
+        else
+            return products;
 
+    }
+
+    public Product getProduct(Long productId) {
+        return products.get((int)productId.longValue() - 1);
     }
 
 }
--- rest-service/src/main/java/de/escalon/rest/ProductController.java
+++ rest-service/src/main/java/de/escalon/rest/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,11 +17,20 @@ public class ProductController {
     private ProductAccess productAccess;
 
     @RequestMapping(value = "/persons/{personId}", method = RequestMethod.GET)
-    public HttpEntity<List<ProductResource>> getProductsOfPerson(Long personId) {
-        Iterable<? extends Product> products = productAccess.getProductsOfPerson(personId);
+    public HttpEntity<List<ProductResource>> getProductsOfPerson(@PathVariable Long personId) {
+        Iterable<? extends Product> products = productAccess
+                .getProductsOfPerson(personId);
         ProductResourceAssembler assembler = new ProductResourceAssembler();
         List<ProductResource> resources = assembler.toResources(products);
         return new HttpEntity<List<ProductResource>>(resources);
+    }
+
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    public HttpEntity<ProductResource> show(@PathVariable Long productId) {
+        Product product = productAccess.getProduct(productId);
+        ProductResourceAssembler assembler = new ProductResourceAssembler();
+        ProductResource resource = assembler.toResource(product);
+        return new HttpEntity<ProductResource>(resource);
     }
 
 }
